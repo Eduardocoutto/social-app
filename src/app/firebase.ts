2aeb5dc810ce0982011822d3d3f95b8e8c4f0515
Eduardo Couto Rodrigues <eduardocoouto@gmail.com>
2019-04-24 11:01:23 -0300

    Registro e Login FireBase

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyDQj8Ch-Vlfr03mOcrXY0UBGM26lHT3taA",
+    authDomain: "virtualwaiter-teste.firebaseapp.com",
+    databaseURL: "https://virtualwaiter-teste.firebaseio.com",
+    projectId: "virtualwaiter-teste",
+    storageBucket: "virtualwaiter-teste.appspot.com",
+    messagingSenderId: "134291755653"
+  };
+
+  export default config
